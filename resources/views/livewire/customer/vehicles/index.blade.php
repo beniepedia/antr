1492adbf278449f7624c931493a40e708d881ec3
beniepedia <beniepedia@gmail.com>
2025-09-28 20:15:41 +0700

    desain dashboard

--- resources/views/livewire/customer/vehicles/index.blade.php
+++ resources/views/livewire/customer/vehicles/index.blade.php
@@ -1,24 +1,25 @@
   <div class="relative max-w-2xl mx-auto">
 
       <!-- Header -->
-      <div class="flex items-center justify-start gap-6 mb-6">
-          <a href="{{ route('customer.dashboard') }}" class="text-blue-600 hover:text-blue-800">
+      <div class="flex items-center gap-6 mt-5 mb-8 px-3">
+          <a href="{{ route('customer.dashboard') }}" class="text-blue-600 hover:text-blue-800" wire:navigate>
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
               </svg>
           </a>
-          <h1 class="text-xl font-bold text-gray-800">Kendaraan Saya</h1>
+          <h1 class="text-xl font-semibold text-gray-900 absolute left-1/2 transform -translate-x-1/2">Kendaraan Saya
+          </h1>
       </div>
 
       <!-- Vehicles List -->
       @if ($vehicles->count() > 0)
-          <div class="space-y-4">
+          <div class="space-y-4 px-3">
               @foreach ($vehicles as $vehicle)
-                  <div class="bg-white/90 backdrop-blur-sm rounded-2xl shadow-lg border border-white/20 px-3 py-4">
+                  <div class="bg-white/90 backdrop-blur-sm rounded-2xl shadow-md border border-white/20 px-3 py-4">
                       <div class="flex items-center justify-between">
                           <div class="flex items-center">
                               <div
-                                  class="w-12 h-12 bg-gradient-to-br from-blue-400 to-purple-500 rounded-full flex items-center justify-center mr-4">
+                                  class="w-12 h-12 bg-gradient-to-br from-blue-300 to-purple-400 rounded-full flex items-center justify-center mr-4 ">
                                   <span class="icon-[tabler--car] text-white size-6"></span>
                               </div>
                               <div>
